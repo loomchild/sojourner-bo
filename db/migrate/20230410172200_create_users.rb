11fffcfd071d51f1ce0ad7cfdd25c3c4709333eb
Jarek Lipski <loomchild@loomchild.net>
2023-04-10 20:11:07 +0200

    postgres / users

--- db/migrate/20230410172200_create_users.rb
+++ db/migrate/20230410172200_create_users.rb
@@ -1,11 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
-      t.string :uid
+    create_table :users, id: :string do |t|
       t.string :email
 
       t.timestamps
     end
-    add_index :users, :uid, unique: true
   end
 end
