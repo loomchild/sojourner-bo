64bb3139180c928bcf52cc75dab79130ba7fabf9
Jarek Lipski <loomchild@loomchild.net>
2023-04-16 11:35:51 +0200

    comment-out Firebase stuff

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,12 @@ require 'google/cloud/firestore'
 
 class DashboardController < ApplicationController
   def index
-    client = Google::Cloud::Firestore.new
-    path = Google::Cloud::Firestore::FieldPath.new(:'fosdem-2023', :favourites)
-    collection = client.col 'users'
-    query = collection.where(path, '!=', []).limit(1)
-    query.get do |user|
-      @favourites = user.data.dig(:'fosdem-2023', :favourites)
-    end
+    #client = Google::Cloud::Firestore.new
+    #path = Google::Cloud::Firestore::FieldPath.new(:'fosdem-2023', :favourites)
+    #collection = client.col 'users'
+    #query = collection.where(path, '!=', []).limit(1)
+    #query.get do |user|
+    #  @favourites = user.data.dig(:'fosdem-2023', :favourites)
+    #end
   end
 end
