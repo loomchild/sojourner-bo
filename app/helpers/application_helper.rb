e8238940b4b6bc5ffe3e48b06f0bde2fac72762f
Jarek Lipski <loomchild@loomchild.net>
2023-04-16 19:15:17 +0200

    dynamic dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     current_classes = current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
     link_to label, path, class: "rounded-md px-3 py-2 text-sm font-medium #{current_classes}", 'aria_current': current ? 'page' : nil
   end
+
+  def dropdown_link(label, path = nil)
+    link_to label, path, class: "text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900", role: "menuitem"
+  end
 end
