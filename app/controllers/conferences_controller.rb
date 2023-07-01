477de64cdf66f9d6ba15deb80f8ef6de799e647b
Jarek Lipski <loomchild@loomchild.net>
2023-07-01 19:26:38 +0200

    add histogram

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -12,6 +12,9 @@ class ConferencesController < ApplicationController
     @active_user_count_increase = @conference.users.active.where(created_at: INCREASE_SINCE..).count
 
     @favourite_count = @conference.favourites.count
+    @favourite_count_average = @user_count.positive? ? (@conference.favourites.count.to_f / @user_count) : 0
+
+    @favourite_histogram_data = favourite_histogram_data
   end
 
   def events
@@ -29,5 +32,12 @@ class ConferencesController < ApplicationController
 
   def set_conferences
     @conferences = Conference.by_name
+  end
+
+  def favourite_histogram_data
+    data = @conference.users.active.group(:favourites_count).count
+    max = (data.keys.max / 10.0).ceil * 10
+    data.keys.min.upto(max) { |k| data[k] = 0 if data[k].nil? }
+    data
   end
 end
