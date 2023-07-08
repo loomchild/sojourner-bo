d95cc29832e0ec20d8d6a39a860bf42b11193434
Jarek Lipski <loomchild@loomchild.net>
2023-07-08 19:56:34 +0200

    remove over 30 days, add events and tracks

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -6,15 +6,16 @@ class ConferencesController < ApplicationController
 
   def show
     @active_user_count = @conference.users.active.count
-    @active_user_count_increase = @conference.users.active.where(created_at: INCREASE_SINCE..).count
 
     @favourite_count = @conference.users.active.sum(:favourites_count).ceil
-    @favourite_count_increase = 0  # TODO: need to add dates to favorites first
 
     @favourite_count_average = @active_user_count.positive? ? (@favourite_count.to_f / @active_user_count) : 0
 
     @event_count = @conference.events.count
     @event_favourite_count_average = @event_count.positive? ? (@favourite_count.to_f / @event_count) : 0
+
+    @track_count = @conference.tracks.count
+    @track_favourite_count_average = @track_count.positive? ? (@favourite_count.to_f / @track_count) : 0
 
     @favourite_histogram_data = favourite_histogram_data
 
