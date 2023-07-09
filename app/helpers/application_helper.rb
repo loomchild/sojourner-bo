f2dd751327aeec98da700735610f6bbd32c481f5
Jarek Lipski <loomchild@loomchild.net>
2023-07-09 19:43:32 +0200

    simple responsiveness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,11 @@
 module ApplicationHelper
   def navbar_link(label, path)
     current = current_page?(path)
-    current_classes = current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-    link_to label, path, class: "rounded-md px-3 py-2 text-sm font-medium #{current_classes}", 'aria_current': current ? 'page' : nil
+
+    classes = 'rounded-md px-1 md:px-3 py-1 md:py-2 text-sm font-medium ' + (current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white')
+    label = current ? content_tag(:span, label) : content_tag(:span, label, class: 'hidden sm:inline') + content_tag(:span, '•', class: 'sm:hidden')
+
+    link_to label, path, class: classes, 'aria_current': current ? 'page' : nil
   end
 
   def dropdown_link(label, path)
