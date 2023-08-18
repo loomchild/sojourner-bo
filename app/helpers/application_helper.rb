9300eb70a54038301513527f374ceab083de9b9b
Jarek Lipski <loomchild@loomchild.net>
2023-08-18 15:58:31 +0200

    sojourner link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,9 @@ module ApplicationHelper
 
     classes = 'rounded-md px-1 md:px-3 py-1 md:py-2 text-sm font-medium ' + (current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white')
 
-    link_to label, path, class: classes, 'aria_current': current ? 'page' : nil
+    target = '_blank' if path.respond_to?(:start_with?) and path.start_with?('http')
+
+    link_to label, path, class: classes, 'aria_current': current ? 'page' : nil, target:
   end
 
   def dropdown_link(label, path)
