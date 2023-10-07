e59deac9308ff873baecf89be41d270b97680006
Jarek Lipski <loomchild@loomchild.net>
2023-10-07 14:24:49 +0200

    upgrade rails

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,23 @@ Bundler.require(*Rails.groups)
 
 module SojournerBo
   class Application < Rails::Application
-    config.load_defaults 7.0
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
 
     config.firebase = config_for(:firebase)
     config.firebase.token = ENV['FIREBASE_ADMIN_TOKEN']
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
