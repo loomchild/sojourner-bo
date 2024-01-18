e67da47f562870ee6ef7dc17bc5f483daa23d0a1
Jarek Lipski <loomchild@loomchild.net>
2024-01-18 19:34:33 +0100

    show absolute time, fix TZ to brussels

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module SojournerBo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "Europe/Brussels"
   end
 end
