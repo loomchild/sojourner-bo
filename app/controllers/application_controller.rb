3474726b40de47891d7b043df327490a11f71c79
Jarek Lipski <loomchild@loomchild.net>
2024-11-09 17:14:24 +0100

    Upgrade to Rails 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  allow_browser versions: :modern
+
   # Overriding default Turbo::Frames::FrameRequest. Frame layout is provided conditionally in each layout.
   layout nil
   helper_method :turbo_frame_request?
