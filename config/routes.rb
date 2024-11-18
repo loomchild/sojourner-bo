3528f20f27c7705850d4cd96d586ddf7b58284fb
Jarek Lipski <loomchild@loomchild.net>
2024-11-18 01:52:27 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
   root 'root#index'
 
   resources :conferences, param: :conference_id, only: [:show] do
