466114744873a33057f1fa71fbddca28ba4e5728
Jarek Lipski <loomchild@loomchild.net>
2024-12-01 21:27:13 +0100

    try fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins '*'
-      resource '/public/conferences/*', headers: :any, methods: [:get]
+      resource '/conferences/*', headers: :any, methods: [:get]
     end
   end
 end
