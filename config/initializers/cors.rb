f9ff37b7f2d6c78022f89739a80a27c7068870a9
Jarek Lipski <loomchild@loomchild.net>
2025-12-21 18:31:07 +0100

    CORS local

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
-if Rails.env.production?
-  Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-      resource '/conferences/*', headers: :any, methods: [:get]
-    end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '/conferences/*', headers: :any, methods: [:get]
   end
 end
